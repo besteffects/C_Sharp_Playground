81f00f7b52c7b56172c5e166187e7719cde5769d
Vit <besteffects@gmail.com>
2021-04-18 15:41:44 -0400

    Added selenium extensions

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -4,20 +4,22 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-class Tests
-{
-    static void Main()
+namespace C_Sharp_Playground.Tests {
+    class Tests
     {
-        using (IWebDriver driver = new ChromeDriver())
+        static void Main()
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            driver.Navigate().GoToUrl("https://www.google.com/ncr");
-            driver.FindElement(By.Name("q")).SendKeys("cheese" + Keys.Enter);
-            wait.Until(webDriver => webDriver.FindElement(By.CssSelector("h3")).Displayed);
-            IWebElement firstResult = driver.FindElement(By.CssSelector("h3"));
-            Console.WriteLine(firstResult.GetAttribute("textContent"));
-            driver.Close();
-            driver.Quit();
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                driver.Navigate().GoToUrl("https://www.google.com/ncr");
+                driver.FindElement(By.Name("q")).SendKeys("cheese" + Keys.Enter);
+                wait.Until(webDriver => webDriver.FindElement(By.CssSelector("h3")).Displayed);
+                IWebElement firstResult = driver.FindElement(By.CssSelector("h3"));
+                Console.WriteLine(firstResult.GetAttribute("textContent"));
+                driver.Close();
+                driver.Quit();
+            }
         }
     }
 }
